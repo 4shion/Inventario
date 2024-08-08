500cefad32ed5f08f88c5fb9c5ec22d9ca7e02fc
Shiro <emmanuelalarcon2006@gmail.com>
2024-08-08 15:35:02 -0400

    Eliminacion de importaciones innecesarias

--- src/main/java/com/mycompany/inventario/MateriaController.java
+++ src/main/java/com/mycompany/inventario/MateriaController.java
@@ -7,35 +7,25 @@ import com.mycompany.inventario.campos.materia;
 import com.mycompany.inventario.campos.proveedor;
 import java.io.IOException;
 import java.net.URL;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import javafx.util.Callback;
 
 /**
  * FXML Controller class
