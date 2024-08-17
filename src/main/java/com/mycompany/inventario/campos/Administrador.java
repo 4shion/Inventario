baace7172119c2ff10a39b98d42f05f05f968467
Shiro <emmanuelalarcon2006@gmail.com>
2024-08-16 21:04:57 -0400

    Inicio de login

---
+++ src/main/java/com/mycompany/inventario/campos/Administrador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inventario.campos;
+
+/**
+ *
+ * @author User
+ */
+public class Administrador {
+    
+    private int cod;
+    private String nombre;
+    private String correo;
+    private int codAdmi;
+    
+}
